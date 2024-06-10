59c70efd6d1caff60f3aa1dc5b744587b31d1a89
Mennorefaat <bmennarefaat84@gmail.com>
2024-06-10 14:46:51 +0300

    change flavors

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:task/app.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.blue, // navigation bar color
+    statusBarColor: Colors.pink, // status bar color
+  ));
   runApp(const MyApp());
 }
 
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:task/app.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.yellow, // navigation bar color
+    statusBarColor: Colors.green, // status bar color
+  ));
   runApp(const MyApp());
 }
 
